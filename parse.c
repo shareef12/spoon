8f05076b60cb6385af66e005711f6522ffaf841a
shareef12 <shar33f12@gmail.com>
2013-02-01 13:12:04 -0500

    replaced byte by byte pattern-matching with buffered input

--- parse.c
+++ parse.c
@@ -1,64 +1,76 @@
-#include <stdio.h>
 #include "signatures.c"
 
-void extract_to_footer(FILE *img, struct signature_s *ftype) {
-    char *outfile = (char *) malloc(16);
-    char byte;
-    int i, inside = 1, pos = ftell(img);
+void extract_to_footer(FILE *img, struct signature_s *sig) {
+    char buffer[16384], *outfile = (char *) malloc(16);
+    int read, offset, pos = ftell(img);
     FILE *out;
 
-    sprintf(outfile, "%d%s", pos, ftype->extension);
+    sprintf(outfile, "%d%s", pos, sig->extension);
     out = fopen(outfile, "w");
 
-    printf("%s found at position %d. Extracting to %s...\n", ftype->extension, pos, outfile);
-    while (inside) {
-        inside = 0;
-        for (i=0; i<strlen(ftype->footer); i++) {
-            if (fread(&byte, 1, 1, img) == 0) {
-                puts("  --EOF reached, no file footer found");
-                break;
-            }
-            fwrite(&byte, 1, 1, out);
-            if (byte != ftype->footer[i]) {
-                inside = 1;
-                break;
-            }
+    printf("%s found at position %d. Extracting to %s...\n", sig->extension, pos, outfile);
+    
+    while ((read = fread(buffer, 1, 16384, img)) != 0) {
+        if ((offset = match_sequence(buffer, read, sig->footer)) == 16384) {
+            fwrite(&buffer, 1, 16384 - 2048, out);
+            fseek(img, -2048, SEEK_CUR);
+        }
+        else {
+            fwrite(&buffer, 1, offset + strlen(sig->footer), out);
+            break;
         }
     }
+    
     fseek(img, pos+1, SEEK_SET);
 }
 
-int parser_parse(FILE *img, struct signatures_s *sigs) {
-    char byte;
-    int i, pos, match;
-    struct signature_s *sig;
 
-    while (fread(&byte, 1, 1, img) != 0) {
-        pos = ftell(img)-1;
-        sig = sigs->first;
-        while (sig != NULL) {
-            match = 1;
-            for (i=0; i<strlen(sig->header); i++) {
-                if (byte == sig->header[i]) {
-                    fread(&byte, 1, 1, img);
-                    continue;
-                }
-                else {
-                    match = 0;
-                    break;
-                }
+int match_sequence(char *buffer, int buflen, char *sequence) {
+    int i, j, found, offset;
+    
+    if (buflen > 16384 - 2048)
+        buflen = 16384 - 2048;
+        
+    for (i=0; i < buflen; i++) {
+        found = 1;
+        for (j=0; j < strlen(sequence); j++) {
+            if (buffer[i+j]  !=  sequence[j]) {
+                found = 0;
+                break;
             }
+        }
+            
+        if (found) {
+            return i;
+        }
+    }
 
-            fseek(img, pos, SEEK_SET);
+    return 16384;
+}
 
-            if (match) {
+
+int parser_parse(FILE *img, struct signatures_s *sigs) {
+    char buf[16384];
+    int i, read, offset, match = 0;
+    struct signature_s *sig = sigs->first;
+
+    while ((read = fread(buf, 1, 16384, img)) != 0) {
+        while (sig != NULL) {
+            if ((offset = match_sequence(buf, read, sig->header)) != 16384) {
+                match = 1;
+                fseek(img, offset - read, SEEK_CUR);
                 extract_to_footer(img, sig);
                 break;
             }
-            
-            fread(&byte, 1, 1, img);
-            sig = sig->next;
+            else {
+                sig = sig-> next;
+            }
         }
+        if (!match)
+            fseek(img, -2048, SEEK_CUR);
+
+        sig = sigs->first;
     }
+
     return 0;
 }
--- spoon.c
+++ spoon.c
@@ -1,12 +1,17 @@
 #include "arg_parse.c"
 #include "parse.c"
+
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void main(int argc, char* argv[]) {                                                                                                                                                                        
     struct arguments* args = parse_args(argc, argv);
-    printf("Running default forensics on: %s\n", args->inPath);
-    FILE *fp;
     struct signatures_s *sigs = retrieve_signatures();
+    FILE *fp;
+
+    printf("Running default forensics on: %s\n", args->inPath);
+
     fp = fopen(args->inPath, "rb");
     parser_parse(fp, sigs);
     fclose(fp);
